56f15d81762a49e3c1f72f982e1da814ba18f56c
idzaaa9 <maliodlinka@gmail.com>
2024-07-30 11:34:12 +0200

    refactor: changed the dumping logic

--- lib/write-ahead-log/WriteAheadLog.go
+++ lib/write-ahead-log/WriteAheadLog.go
@@ -1,17 +1,19 @@
 package writeaheadlog
 
 import (
-	hashfunc "NASP_projekat2023/utils"
-	"bytes"
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strconv"
-	"time"
 )
+
+/* TODO:
+- Once memtables and mempool is implemented, we need to expand the WAL to work with multiple memtables,
+  while keeping the data consistent. My suggestion is to have a map[int][]string where the key is the
+	ID of the memtable and the value is all of the segments which are associated with that memtable.
+*/
 
 /*
    +---------------+-----------------+---------------+---------------+-----------------+-...-+--...--+
@@ -26,267 +28,75 @@ import (
    Timestamp = Timestamp of the operation in seconds
 */
 
-const (
-	CRC_SIZE        = 4
-	TIMESTAMP_SIZE  = 8
-	TOMBSTONE_SIZE  = 1
-	KEY_SIZE_SIZE   = 8
-	VALUE_SIZE_SIZE = 8
-
-	CRC_START        = 0
-	TIMESTAMP_START  = CRC_START + CRC_SIZE
-	TOMBSTONE_START  = TIMESTAMP_START + TIMESTAMP_SIZE
-	KEY_SIZE_START   = TOMBSTONE_START + TOMBSTONE_SIZE
-	VALUE_SIZE_START = KEY_SIZE_START + KEY_SIZE_SIZE
-	KEY_START        = VALUE_SIZE_START + VALUE_SIZE_SIZE
-
-	WAL_PUT    = 0
-	WAL_DELETE = 1
-)
-
-type WriteAheadLogEntry struct {
-	// key:value are the only things we need to generate an entry
-	// the rest is metadata which we can generate ourselves
-	Key       []byte
-	Value     []byte
-	Timestamp time.Time
-	Tombstone bool
-}
-
-func NewEntry(key, value []byte, operation int) (*WriteAheadLogEntry, error) {
-	if !(operation == WAL_DELETE || operation == WAL_PUT) {
-		return nil, errors.New("value must be 0 or 1")
-	}
-	if len(key) == 0 {
-		return nil, errors.New("key is an empty array")
-	}
-	if key == nil {
-		return nil, errors.New("key is nil")
-	}
-	if (value == nil || len(value) == 0) && operation != WAL_DELETE {
-		return nil, errors.New("value is not provided while putting an entry")
-	}
-
-	ts := false
-	if operation == WAL_DELETE {
-		ts = true
-	}
-
-	return &WriteAheadLogEntry{
-		key,
-		value,
-		time.Now(),
-		ts,
-	}, nil
-}
-
-func SerializeEntry(entry WriteAheadLogEntry) ([]byte, int) {
-	// returns the serialized entry and the size of it
-	// first we create all of the parts of the serialized entry, and join them in the end
-	crc := make([]byte, CRC_SIZE)
-	timestamp := make([]byte, TIMESTAMP_SIZE)
-	tombstone := make([]byte, TOMBSTONE_SIZE)
-	keysize := make([]byte, KEY_SIZE_SIZE)
-	valuesize := make([]byte, VALUE_SIZE_SIZE)
-
-	binary.BigEndian.PutUint64(timestamp, uint64(entry.Timestamp.Unix()))
-	if entry.Tombstone {
-		tombstone[0] = 1
-	} else {
-		tombstone[0] = 0
-	}
-
-	binary.BigEndian.PutUint64(keysize, uint64(len(entry.Key)))
-	binary.BigEndian.PutUint64(valuesize, uint64(len(entry.Value)))
-
-	returnArray := append(timestamp, tombstone...)
-	returnArray = append(returnArray, keysize...)
-	returnArray = append(returnArray, valuesize...)
-	returnArray = append(returnArray, entry.Key...)
-	returnArray = append(returnArray, entry.Value...)
-
-	crc = hashfunc.Crc32AsBytes(returnArray)
-
-	returnArray = append(crc, returnArray...)
-
-	return returnArray, len(returnArray)
-}
-
-func deserializeEntry(data []byte) (*WriteAheadLogEntry, []byte, error) {
-	reader := bytes.NewReader(data)
-
-	crc := make([]byte, CRC_SIZE)
-	timestampBytes := make([]byte, TIMESTAMP_SIZE)
-	tombstone := make([]byte, TOMBSTONE_SIZE)
-	keysize := make([]byte, KEY_SIZE_SIZE)
-	valuesize := make([]byte, VALUE_SIZE_SIZE)
-
-	err := binary.Read(reader, binary.BigEndian, &crc)
-	if err != nil {
-		return nil, nil, errors.New("error while reading crc from a wal entry")
-	}
-	err = binary.Read(reader, binary.BigEndian, &timestampBytes)
-	if err != nil {
-		return nil, nil, errors.New("error while reading timestamp from a wal entry")
-	}
-	if _, err = reader.Read(tombstone); err != nil {
-		return nil, nil, err
-	}
-	err = binary.Read(reader, binary.BigEndian, &keysize)
-	if err != nil {
-		return nil, nil, errors.New("error while reading keysize from a wal entry")
-	}
-	err = binary.Read(reader, binary.BigEndian, &valuesize)
-	if err != nil {
-		return nil, nil, errors.New("error while reading valuesize from a wal entry")
-	}
-
-	timestamp := time.Unix(int64(binary.BigEndian.Uint64(timestampBytes)), 0)
-
-	key := make([]byte, binary.BigEndian.Uint64(keysize))
-	value := make([]byte, binary.BigEndian.Uint64(valuesize))
-
-	if _, err := reader.Read(key); err != nil {
-		return nil, nil, err
-	}
-
-	if _, err := reader.Read(value); err != nil {
-		return nil, nil, err
-	}
-
-	entry := &WriteAheadLogEntry{
-		Timestamp: timestamp,
-		Tombstone: tombstone[0] == 1,
-		Key:       key,
-		Value:     value,
-	}
-
-	return entry, crc, nil
-}
-
-type WALBuffer struct {
-	Data []WriteAheadLogEntry
-	Size int // size defines number of entries
-}
-
-func NewWALBuffer(size int) *WALBuffer {
-	data := make([]WriteAheadLogEntry, size)
-	return &WALBuffer{
-		Data: data,
-		Size: size,
-	}
-}
-
 type WriteAheadLog struct {
-	// since the WAL is segmented, we will need to keep track of segment indexes
 	CurrentFile    *os.File
-	Index          int
-	SegmentSize    int
+	Index          int // last(current) wal segment
+	First          int // first wal segment
+	SegmentSize    int // segment size in bytes
 	Buffer         WALBuffer
 	BytesRemaining int    // remaining bytes from the last segment
 	Path           string // contains path to the WAL folder
 }
 
+/*
+this function will go through the folder with the WAL data, and return the
+largest value of a segment
+ALL OF THE WAL FILE NAMES MUST BE IN THE FORMAT "wal_00001.log" !!!!!!!!!!
+*/
 func ScanWALFolder(path string) (int, error) {
-	/* this function will go through the folder with the WAL data, and return the
-	   largest value of a segment
-	   ALL OF THE WAL FILE NAMES MUST BE IN THE FORMAT "wal_00001.log" !!!!!!!!!!*/
-
 	files, err := os.ReadDir(path)
 	if err != nil {
 		return -1, err
 	}
 
 	// this regex returns all of the files which match our format
-	re := regexp.MustCompile(`wal_(\d{5})\.log`)
+	re := regexp.MustCompile(`^wal_(\d{5})\.log$`)
 
-	highestIndex := 0
+	maxIndex := 0
+	minIndex := 999999999
 
 	for _, file := range files {
-		reMatch := re.FindAllStringSubmatch(file.Name(), -1)
-		if reMatch != nil && len(reMatch) == 2 {
-			currIndex, err := strconv.Atoi(reMatch[1][0])
-			if err == nil {
-				if currIndex > highestIndex {
-					highestIndex = currIndex
-				}
-			}
+		if file.IsDir() {
+			continue
 		}
-	}
+		filename := file.Name()
 
-	return highestIndex, nil
-}
+		// Match the filename against the regex pattern
+		matches := re.FindStringSubmatch(filename)
 
-func (wal *WriteAheadLog) Dump() error {
-	if wal.CurrentFile == nil {
-		wal.Index++
-		filename := fmt.Sprintf("wal_%05d.log", wal.Index)
-		filePath := filepath.Join(wal.Path, filename)
+		if len(matches) <= 1 {
+			continue
+		}
 
-		file, err := os.Create(filePath)
+		indexStr := matches[1]
+		// Convert the index to an integer
+		index, err := strconv.Atoi(indexStr)
 		if err != nil {
-			return err
+			return 0, err
 		}
-		wal.CurrentFile = file
-	}
 
-	for i := 0; i < wal.Buffer.Size; i++ {
-		entry := wal.Buffer.Data[i]
-
-		serializedEntry, entrySize := SerializeEntry(entry)
-		if wal.BytesRemaining < entrySize {
-			if _, err := wal.CurrentFile.Write(serializedEntry[:wal.BytesRemaining]); err != nil {
-				return err
-			}
-
-			if err := wal.CurrentFile.Close(); err != nil {
-				return err
-			}
-
-			wal.Index++
-			filename := fmt.Sprintf("wal_%05d.log", wal.Index)
-			filePath := filepath.Join(wal.Path, filename)
-
-			file, err := os.Create(filePath)
-			if err != nil {
-				return err
-			}
-
-			wal.CurrentFile = file
-
-			breakBytes := make([]byte, 4)
-			binary.BigEndian.PutUint32(breakBytes, uint32(entrySize-wal.BytesRemaining))
-			if _, err := wal.CurrentFile.Write(breakBytes); err != nil {
-				return err
-			}
-
-			if _, err := wal.CurrentFile.Write(serializedEntry[wal.BytesRemaining:]); err != nil {
-				return err
-			}
-
-			// we create a new segment, decrease the 4 bytes which represent number of bytes from the last entry
-			wal.BytesRemaining = wal.SegmentSize - entrySize + wal.BytesRemaining - 4
-		} else {
-			if wal.BytesRemaining == wal.SegmentSize {
-				breakBytes := make([]byte, 4)
-				binary.BigEndian.PutUint32(breakBytes, 0)
-				if _, err := wal.CurrentFile.Write(breakBytes); err != nil {
-					return err
-				}
-				wal.BytesRemaining -= 4
-			}
-			if _, err := wal.CurrentFile.Write(serializedEntry); err != nil {
-				return err
-			}
-			wal.BytesRemaining -= entrySize
+		// Update the maximum index found
+		if index > maxIndex {
+			maxIndex = index
+		} else if index < minIndex {
+			minIndex = index
 		}
 	}
 
-	wal.Buffer.Data = make([]WriteAheadLogEntry, wal.Buffer.Size)
-	return nil
+	return maxIndex, nil
 }
 
-func NewWriteAheadLog(filepath string, segmentSize, bufferSize int) (*WriteAheadLog, error) {
+// creates the work directory for the WAL if it doesn't exist
+func createWorkDir(filepath string) error {
+	_, err := os.Stat(filepath)
+	if os.IsNotExist(err) {
+		err = os.MkdirAll(filepath, 0755)
+	}
+	return err
+}
+
+func NewWriteAheadLog(filepath string, segmentSize int) (*WriteAheadLog, error) {
+	err := createWorkDir(filepath)
 	index, err := ScanWALFolder(filepath)
 
 	if err != nil {
@@ -297,20 +107,95 @@ func NewWriteAheadLog(filepath string, segmentSize, bufferSize int) (*WriteAhead
 		CurrentFile:    nil,
 		Index:          index,
 		SegmentSize:    segmentSize,
-		Buffer:         *NewWALBuffer(bufferSize),
+		Buffer:         *NewWALBuffer(segmentSize),
 		BytesRemaining: segmentSize,
 		Path:           filepath,
 	}, nil
 }
 
+// creates a new segment file
+func (wal *WriteAheadLog) createNewSegment() error {
+	wal.Index++
+	segmentName := fmt.Sprintf("wal_%05d.log", wal.Index)
+	segmentPath := filepath.Join(wal.Path, segmentName)
+
+	file, err := os.Create(segmentPath)
+	if err != nil {
+		return err
+	}
+	wal.CurrentFile = file
+	return nil
+}
+
+// writes the current state of the buffer to the disk
+func (wal *WriteAheadLog) dump() error {
+	if wal.CurrentFile == nil {
+		wal.Index++
+		segmentName := fmt.Sprintf("wal_%05d.log", wal.Index)
+		segmentPath := filepath.Join(wal.Path, segmentName)
+
+		file, err := os.Create(segmentPath)
+		if err != nil {
+			return err
+		}
+		wal.CurrentFile = file
+	}
+
+	for i := 0; i < len(wal.Buffer.Data); i++ {
+		entry := wal.Buffer.Data[i]
+
+		serializedEntry := entry.Serialize()
+
+		if wal.BytesRemaining >= entry.Size {
+			if _, err := wal.CurrentFile.Write(serializedEntry); err != nil {
+				return err
+			}
+			wal.BytesRemaining -= entry.Size
+			continue
+		}
+
+		// write as many bytes as we can fit
+		bytesWritten, err := wal.CurrentFile.Write(serializedEntry[:wal.BytesRemaining])
+		if err != nil {
+			return err
+		}
+
+		// close the current file
+		if err := wal.CurrentFile.Close(); err != nil {
+			return err
+		}
+
+		// create a new segment
+		if err := wal.createNewSegment(); err != nil {
+			return err
+		}
+
+		// write the remaining bytes
+		if _, err := wal.CurrentFile.Write(serializedEntry[entry.Size-bytesWritten:]); err != nil {
+			return err
+		}
+		wal.BytesRemaining = wal.SegmentSize - (entry.Size - bytesWritten)
+	}
+
+	wal.Buffer = *NewWALBuffer(wal.SegmentSize)
+	return nil
+}
+
+func (wal *WriteAheadLog) isRdyToDump() bool {
+	return wal.Buffer.Size >= wal.SegmentSize
+}
+
+// use this method when adding a new entry to the WAL
 func (wal *WriteAheadLog) Log(key, value []byte, operation int) error {
 	entry, err := NewEntry(key, value, operation)
 	if err != nil {
 		return err
 	}
-	wal.Buffer.Data = append(wal.Buffer.Data, *entry)
-	if len(wal.Buffer.Data) == cap(wal.Buffer.Data) {
-		err = wal.Dump()
+	wal.Buffer.Add(*entry)
+
+	if wal.isRdyToDump() {
+		err = wal.dump()
 	}
+
 	return err
 }
