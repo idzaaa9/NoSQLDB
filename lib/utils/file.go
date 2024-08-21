cd701a0e6721a49e6c153997a061efda8d603641
idzaaa9 <maliodlinka@gmail.com>
2024-08-21 16:56:43 +0200

    feat: added helper function for checking empty directories

--- lib/utils/file.go
+++ lib/utils/file.go
@@ -6,3 +6,13 @@ func GetFileSize(file os.File) int {
 	fileInfo, _ := file.Stat()
 	return int(fileInfo.Size())
 }
+
+func IsEmptyDir(path string) bool {
+	dir, _ := os.Open(path)
+	defer dir.Close()
+	_, err := dir.ReadDir(1)
+	if err != nil {
+		return true
+	}
+	return false
+}
