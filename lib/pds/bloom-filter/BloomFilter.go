96b8c9d4e12ff6fcdfb64a265945cac3f46db660
Vuk Vicentic <vuk.vicentic@gmail.com>
2024-08-22 18:34:01 +0200

    added comments for bloom filter

--- lib/pds/bloom-filter/BloomFilter.go
+++ lib/pds/bloom-filter/BloomFilter.go
@@ -6,16 +6,18 @@ import (
 	"os"
 )
 
+// BloomFilter represents a probabilistic data structure for set membership testing.
 type BloomFilter struct {
-	Bitset []bool
-	Size   uint
-	Hashes []utils.HashWithSeed
+	Bitset []bool               // Bitset to store the presence of elements
+	Size   uint                 // Size of the bitset
+	Hashes []utils.HashWithSeed // Array of hash functions with seeds
 }
 
+// NewBloomFilter creates a new Bloom filter with the given expected number of elements and false positive rate.
 func NewBloomFilter(expectedElements int, falsePositiveRate float64) *BloomFilter {
-	m := utils.CalculateM(expectedElements, falsePositiveRate)
-	k := utils.CalculateK(expectedElements, m)
-	hashes := utils.CreateHashFunctions(k)
+	m := utils.CalculateM(expectedElements, falsePositiveRate) // Calculate the size of the bitset
+	k := utils.CalculateK(expectedElements, m)                 // Calculate the number of hash functions
+	hashes := utils.CreateHashFunctions(k)                     // Create the hash functions
 
 	return &BloomFilter{
 		Bitset: make([]bool, m),
@@ -24,6 +26,7 @@ func NewBloomFilter(expectedElements int, falsePositiveRate float64) *BloomFilte
 	}
 }
 
+// Add inserts an element into the Bloom filter.
 func (bf *BloomFilter) Add(element string) {
 	data := []byte(element)
 	for _, hash := range bf.Hashes {
@@ -32,23 +35,26 @@ func (bf *BloomFilter) Add(element string) {
 	}
 }
 
+// Query checks if an element is possibly in the Bloom filter.
 func (bf *BloomFilter) Query(element string) bool {
 	data := []byte(element)
 	for _, hash := range bf.Hashes {
 		index := hash.Hash(data) % uint64(bf.Size)
 		if !bf.Bitset[index] {
-			return false
+			return false // If any bit is not set, the element is definitely not in the set
 		}
 	}
-	return true
+	return true // All bits are set, the element is possibly in the set
 }
 
+// Clear resets the Bloom filter, removing all elements.
 func (bf *BloomFilter) Clear() {
 	for i := range bf.Bitset {
 		bf.Bitset[i] = false
 	}
 }
 
+// Serialize saves the Bloom filter to a file.
 func (bf *BloomFilter) Serialize(filename string) error {
 	file, err := os.Create(filename)
 	if err != nil {
@@ -60,6 +66,7 @@ func (bf *BloomFilter) Serialize(filename string) error {
 	return encoder.Encode(bf)
 }
 
+// Deserialize loads the Bloom filter from a file.
 func (bf *BloomFilter) Deserialize(filename string) error {
 	file, err := os.Open(filename)
 	if err != nil {
