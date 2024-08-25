b580c66052cb79101428529bd5247f298ac8cce1
idzaaa9 <maliodlinka@gmail.com>
2024-08-25 19:52:03 +0200

    refactor: values are now []byte

--- lib/memtable/MapMemtable_test.go
+++ lib/memtable/MapMemtable_test.go
@@ -1,6 +1,7 @@
 package memtable
 
 import (
+	"bytes"
 	"testing"
 )
 
@@ -16,7 +17,7 @@ func TestNewMapMemtable(t *testing.T) {
 
 func TestPut(t *testing.T) {
 	m := NewMapMemtable(10)
-	err := m.Put("key1", "value1")
+	err := m.Put("key1", []byte("value1"))
 	if err != nil {
 		t.Errorf("unexpected error: %v", err)
 	}
@@ -25,7 +26,7 @@ func TestPut(t *testing.T) {
 	if err != nil {
 		t.Errorf("unexpected error: %v", err)
 	}
-	if entry.value != "value1" {
+	if !bytes.Equal(entry.value, []byte("value1")) {
 		t.Errorf("expected value to be 'value1', got '%s'", entry.value)
 	}
 }
@@ -40,7 +41,7 @@ func TestGetNonExistentKey(t *testing.T) {
 
 func TestDelete(t *testing.T) {
 	m := NewMapMemtable(10)
-	err := m.Put("key1", "value1")
+	err := m.Put("key1", []byte("value1"))
 	if err != nil {
 		t.Errorf("unexpected error: %v", err)
 	}
@@ -57,15 +58,15 @@ func TestDelete(t *testing.T) {
 	if !entry.tombstone {
 		t.Errorf("expected entry to be a tombstone, got tombstone %v", entry.tombstone)
 	}
-	if entry.value != "" {
+	if !bytes.Equal(entry.value, []byte("")) {
 		t.Errorf("expected value to be empty, got '%s'", entry.value)
 	}
 }
 
 func TestSize(t *testing.T) {
 	m := NewMapMemtable(10)
-	m.Put("key1", "value1")
-	m.Put("key2", "value2")
+	m.Put("key1", []byte("value1"))
+	m.Put("key2", []byte("value2"))
 	if m.Size() != 2 {
 		t.Errorf("expected size to be 2, got %d", m.Size())
 	}
@@ -76,24 +77,24 @@ func TestSize(t *testing.T) {
 	}
 }
 
-func TestShouldFlush(t *testing.T) {
+func TestIsFull(t *testing.T) {
 	m := NewMapMemtable(2)
-	if m.ShouldFlush() {
-		t.Errorf("expected ShouldFlush to be false when size is 0")
+	if m.IsFull() {
+		t.Errorf("expected IsFull to be false when size is 0")
 	}
 
-	m.Put("key1", "value1")
-	if m.ShouldFlush() {
-		t.Errorf("expected ShouldFlush to be false when size is 1")
+	m.Put("key1", []byte("value1"))
+	if m.IsFull() {
+		t.Errorf("expected IsFull to be false when size is 1")
 	}
 
-	m.Put("key2", "value2")
-	if !m.ShouldFlush() {
-		t.Errorf("expected ShouldFlush to be true when size equals threshold")
+	m.Put("key2", []byte("value2"))
+	if !m.IsFull() {
+		t.Errorf("expected IsFull to be true when size equals threshold")
 	}
 
-	m.Put("key3", "value3")
-	if !m.ShouldFlush() {
-		t.Errorf("expected ShouldFlush to be true when size exceeds threshold")
+	m.Put("key3", []byte("value3"))
+	if !m.IsFull() {
+		t.Errorf("expected IsFull to be true when size exceeds threshold")
 	}
 }
