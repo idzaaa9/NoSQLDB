1542686f23343065f9ddedfb2d7a686330392f11
idzaaa9 <maliodlinka@gmail.com>
2024-09-07 15:52:52 +0200

    feat: updated configurable options

--- lib/config/config.go
+++ lib/config/config.go
@@ -16,16 +16,32 @@ const (
 type Config struct {
 	// BloomFilterFalsePositiveRate float32 `json:"bloom_filter_false_positive_rate"`
 	// BloomFilterExpectedElements  int     `json:"bloom_filter_expected_elements"`
+
+	// WAL
 	WALSegmentSize int    `json:"wal_segment_size"`
-	WALFolder      string `json:"wal_folder"`
+	WALDir         string `json:"wal_folder"`
+
+	// Mempool
+	NumTables        int    `json:"num_tables"`
+	MemtableSize     int    `json:"memtable_size"`
+	SkipListMaxLevel int    `json:"skip_list_max_level"`
+	BTreeMinDegree   int    `json:"btree_min_degree"`
+	OutputDir        string `json:"output_dir"`
+	MemtableType     string `json:"memtable_type"`
 }
 
 // default values go here
 var DefaultConfig = Config{
 	// BloomFilterFalsePositiveRate: 0.2,
 	// BloomFilterExpectedElements:  50000,
-	WALSegmentSize: 64 * KB,
-	WALFolder:      "data/wal/",
+	WALSegmentSize:   64 * KB,
+	WALDir:           "data/wal/",
+	NumTables:        4,
+	MemtableSize:     100,
+	SkipListMaxLevel: 16,
+	BTreeMinDegree:   16,
+	OutputDir:        "data/sstable/",
+	MemtableType:     "map",
 }
 
 func LoadConfig(filepath string) (*Config, error) {
@@ -55,8 +71,34 @@ func LoadConfig(filepath string) (*Config, error) {
 		config.WALSegmentSize = DefaultConfig.WALSegmentSize
 	}
 
-	if config.WALFolder == "" {
-		config.WALFolder = DefaultConfig.WALFolder
+	if config.WALDir == "" {
+		config.WALDir = DefaultConfig.WALDir
+	}
+
+	if config.NumTables <= 0 {
+		config.NumTables = DefaultConfig.NumTables
+	}
+
+	if config.MemtableSize <= 0 {
+		config.MemtableSize = DefaultConfig.MemtableSize
+	}
+
+	if config.SkipListMaxLevel <= 0 {
+		config.SkipListMaxLevel = DefaultConfig.SkipListMaxLevel
+	}
+
+	if config.BTreeMinDegree <= 0 {
+		config.BTreeMinDegree = DefaultConfig.BTreeMinDegree
+	}
+
+	if config.OutputDir == "" {
+		config.OutputDir = DefaultConfig.OutputDir
+	}
+
+	if config.MemtableType != "map" &&
+		config.MemtableType != "btree" &&
+		config.MemtableType != "skip_list" {
+		config.MemtableType = DefaultConfig.MemtableType
 	}
 
 	return &config, err
